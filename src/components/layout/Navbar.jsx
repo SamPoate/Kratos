56216f82750adfa18f896ebaaa5d0ee180442bdb
Sam Poate <sampoate@outlook.com>
2019-10-27 09:17:13 +0000

    Initial Commit

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className='header'>
+      <Link className='logo' to='/'>
+        TSM
+        {/* <img src='img/logos/logo.jpg' alt='The Strength Movement' /> */}
+      </Link>
+      <input className='menu-btn' type='checkbox' id='menu-btn' />
+      <label className='menu-icon' htlmfor='menu-btn'>
+        <span className='navicon'></span>
+      </label>
+      <ul className='menu'>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/'>Workout Plan</Link>
+        </li>
+        <li>
+          <Link to='/'>Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
