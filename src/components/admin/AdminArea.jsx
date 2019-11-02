3cda2b1935496ce178e20105f6a5e2b15d1e3e45
Sam Poate <sampoate@outlook.com>
2019-11-02 18:00:46 +0000

    csv uploading

--- src/components/admin/AdminArea.jsx
+++ src/components/admin/AdminArea.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { withFirebase } from 'react-redux-firebase';
+import { withFirebase, useFirestore } from 'react-redux-firebase';
+import CSVReader from 'react-csv-reader';
 
 const AdminArea = props => {
   const [adminEmail, setAdminEmail] = useState('');
   const [addAdminResponse, setAddAdminResponse] = useState('');
+  const [workoutData, setWorkoutData] = useState([]);
+  const [completed, setCompleted] = useState('');
+  const [phase, setPhase] = useState('');
+  const firestore = useFirestore();
 
   const addAdmin = () => {
     const addAdmin = props.firebase.functions().httpsCallable('addAdminRole');
@@ -16,21 +21,89 @@ const AdminArea = props => {
       .catch(err => console.log(err));
   };
 
+  const handleFile = file => {
+    let key = '';
+    const data = file.reduce((data, row) => {
+      if (!row[0]) {
+        return false;
+      }
+
+      if (row[0].includes('phase')) {
+        setPhase(row[0].toUpperCase());
+        return false;
+      }
+
+      if (row.includes('sets')) {
+        key = row[0];
+        return { ...data, [key]: [] };
+      } else {
+        return {
+          ...data,
+          [key]: [
+            ...data[key],
+            {
+              name: row[0],
+              sets: row[1],
+              reps: row[2],
+              total: row[3],
+              percent: row[4],
+              weight: row[5]
+            }
+          ]
+        };
+      }
+    }, {});
+
+    setWorkoutData(data);
+  };
+
+  const save = () => {
+    firestore
+      .collection('WORKOUT_PROGRAM')
+      .doc(phase)
+      .set(workoutData)
+      .then(() => {
+        setCompleted('File Uploaded');
+      });
+  };
+
   return (
     <main id='admin_area'>
-      <h1>ADMIN AREA</h1>
-      <div className='form-input'>
-        <label>Admin Email</label>
-        <input
-          type='email'
-          value={adminEmail}
-          onChange={e => setAdminEmail(e.target.value)}
-        />
+      <h1>Admin Panel</h1>
+      <div className='content-container'>
+        <div className='card'>
+          <div className='form-input'>
+            <label>Admin Email</label>
+            <input
+              type='email'
+              value={adminEmail}
+              onChange={e => setAdminEmail(e.target.value)}
+            />
+          </div>
+          <button className='submit btn--white-text' onClick={addAdmin}>
+            Add Admin
+          </button>
+          <p className='api-response'>{addAdminResponse}</p>
+        </div>
+        <div className='card'>
+          <CSVReader
+            cssClass='csv-reader-input'
+            label='Upload a .csv'
+            onFileLoaded={handleFile}
+            onError={err => console.log(err)}
+          />
+          <button className='submit btn--white-text' onClick={save}>
+            Save
+          </button>
+          {completed ? (
+            <div className='completed-message'>{completed}</div>
+          ) : null}
+        </div>
+
+        <div className='card full-width'>
+          <h3>User Data</h3>
+        </div>
       </div>
-      <button className='submit btn--white-text' onClick={addAdmin}>
-        Add Admin
-      </button>
-      <p>{addAdminResponse}</p>
     </main>
   );
 };
