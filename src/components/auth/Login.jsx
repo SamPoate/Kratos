a9cb03caeec8340b58c336ae1d674f39c92bedd9
Sam Poate <sampoate@outlook.com>
2019-11-10 10:50:14 +0000

    Better Form Handling

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -3,21 +3,21 @@ import { connect } from 'react-redux';
 import { useFirebase, isEmpty, isLoaded } from 'react-redux-firebase';
 import Loading from '../layout/Loading';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const Login = ({ auth, history }) => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
   const firebase = useFirebase();
 
-  const onLogin = () => {
+  const onLogin = values => {
     setLoading(true);
     firebase
       .auth()
-      .signInWithEmailAndPassword(username, password)
+      .signInWithEmailAndPassword(values.email, values.password)
       .then(() => {
         setLoading(false);
+
         history.push('/');
       })
       .catch(function(error) {
@@ -34,31 +34,50 @@ const Login = ({ auth, history }) => {
       {!isLoaded(auth) || loading ? (
         <Loading />
       ) : isEmpty(auth) ? (
-        <div className='form'>
-          <div className='form-input'>
-            <label>Username</label>
-            <input
-              type='text'
-              value={username}
-              onChange={e => setUsername(e.target.value)}
-            />
-          </div>
-          <div className='form-input'>
-            <label>Password</label>
-            <input
-              type='password'
-              value={password}
-              onChange={e => setPassword(e.target.value)}
-            />
-          </div>
-          <p>{errorMessage}</p>
-          <button className='btn--white-text' onClick={onLogin}>
-            Login
-          </button>
+        <>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validate={values => {
+              const errors = {};
+              if (!values.email) {
+                errors.email = 'Required';
+              } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+              ) {
+                errors.email = 'Invalid email address';
+              }
+              if (!values.password) {
+                errors.password = 'Required';
+              }
+              return errors;
+            }}
+            onSubmit={values => onLogin(values)}
+          >
+            <Form className='form'>
+              <div className='form-input'>
+                <label>Email</label>
+                <Field type='text' name='email' />
+                <ErrorMessage name='email' component='span' />
+              </div>
+              <div className='form-input'>
+                <label>Password</label>
+                <Field type='password' name='password' />
+                <ErrorMessage name='password' component='span' />
+              </div>
+              <p>{errorMessage}</p>
+              <button
+                type='submit'
+                className='btn--white-text'
+                disabled={loading}
+              >
+                Login
+              </button>
+            </Form>
+          </Formik>
           <button className='btn--white-text'>
             <Link to='/register'>Register</Link>
           </button>
-        </div>
+        </>
       ) : null}
     </main>
   );
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,76 +1,94 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useFirebase } from 'react-redux-firebase';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const Register = ({ history }) => {
-  const [email, setEmail] = useState('');
-  const [displayName, setDisplayName] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
   const firebase = useFirebase();
 
-  const onSubmit = () => {
-    if (password === confirmPassword) {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(email, password)
-        .then(cred => {
-          cred.user.updateProfile({
-            displayName: displayName
-          });
-          history.push('/');
-        })
-        .catch(function(error) {
-          console.log(error.code, error.message);
+  const onSubmit = values => {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(values.email, values.password)
+      .then(async cred => {
+        await cred.user.updateProfile({
+          displayName: values.displayName
         });
-    }
+        history.push('/');
+      })
+      .catch(function(error) {
+        console.log(error.code, error.message);
+      });
   };
 
   return (
     <div id='register'>
-      <div className='form'>
-        <div className='form__input'>
-          <label>E-mail</label>
-          <input
-            type='email'
-            name='email'
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-        </div>
-        <div className='form__input'>
-          <label>Display Name</label>
-          <input
-            type='text'
-            name='name'
-            value={displayName}
-            onChange={e => setDisplayName(e.target.value)}
-          />
-        </div>
-        <div className='form__input'>
-          <label>Password</label>
-          <input
-            type='password'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-        <div className='form__input'>
-          <label>Confirm Password</label>
-          <input
-            type='password'
-            value={confirmPassword}
-            onChange={e => setConfirmPassword(e.target.value)}
-          />
-        </div>
-        <button className='btn--white-text' onClick={onSubmit}>
-          Create Account
-        </button>
-        <p>
-          By creating an account you agree to our{' '}
-          <Link to='/about'> Privacy Policy.</Link>
-        </p>
-      </div>
+      <Formik
+        initialValues={{
+          email: '',
+          displayName: '',
+          password: '',
+          confirmPassword: ''
+        }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+
+          if (!values.displayName) {
+            errors.displayName = 'Required';
+          }
+
+          if (!values.password) {
+            errors.password = 'Required';
+          } else if (values.password.length < 6) {
+            errors.password = 'Password must be at least 6 characters long';
+          }
+
+          if (!values.confirmPassword) {
+            errors.confirmPassword = 'Required';
+          } else if (values.password !== values.confirmPassword) {
+            errors.confirmPassword = 'Passwords must match';
+          }
+          return errors;
+        }}
+        onSubmit={values => onSubmit(values)}
+      >
+        <Form className='form'>
+          <div className='form__input'>
+            <label>E-mail</label>
+            <Field type='email' name='email' />
+            <ErrorMessage name='email' component='span' />
+          </div>
+          <div className='form__input'>
+            <label>Display Name</label>
+            <Field type='text' name='displayName' />
+            <ErrorMessage name='displayName' component='span' />
+          </div>
+          <div className='form__input'>
+            <label>Password</label>
+            <Field type='password' name='password' />
+            <ErrorMessage name='password' component='span' />
+          </div>
+          <div className='form__input'>
+            <label>Confirm Password</label>
+            <Field type='password' name='confirmPassword' />
+            <ErrorMessage name='confirmPassword' component='span' />
+          </div>
+          <button type='submit' className='btn--white-text'>
+            Create Account
+          </button>
+        </Form>
+      </Formik>
+      <p>
+        By creating an account you agree to our{' '}
+        <Link to='/about'> Privacy Policy.</Link>
+      </p>
     </div>
   );
 };
