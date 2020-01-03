e69527f48b6046e463b213cb85dd1e640c456008
Sam Poate <sampoate@outlook.com>
2020-01-03 15:45:23 +0000

    Home page menu

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const Home = () => {
+const Home = props => {
   return (
     <div className='home'>
       <div className='logo-container'>
         <img src='img/logos/logo.jpg' alt='The Strength Movement' />
       </div>
-      <div className='about'>
+      {props.authenticated ? (
+        <div className='home__menu-grid'>
+          <Link to='/profile'>
+            <div className='home__menu-grid__menu-button'>Profile</div>
+          </Link>
+          <Link to='/workout'>
+            <div className='home__menu-grid__menu-button'>Workout Plan</div>
+          </Link>
+        </div>
+      ) : null}
+      <div className='home__about'>
         <Link to='/about'>Privacy Policy</Link>
       </div>
     </div>
   );
 };
 
-export default Home;
+const mapStateToProps = state => ({
+  authenticated: state.firebase.auth.uid
+});
+
+export default connect(mapStateToProps)(Home);
